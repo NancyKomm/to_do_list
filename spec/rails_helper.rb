f2cd2184e3e597a1c029b72f1f4a2ca16ae51122
NancyKomm <nancygianna11@gmail.com>
2022-02-14 17:03:09 +0200

    Generated todos controller and test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 # require database cleaner at the top level
-require 'database_cleaner'
+require 'database_cleaner-active_record'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -22,7 +22,8 @@ require 'rspec/rails'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+#Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -48,6 +49,7 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.include RequestSpecHelper, type: :request
 
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
